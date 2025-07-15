a52dc7a3dd1066f8f4c0ee041aa63cc29f2e450e
anonymous <68572566+kinsingo@users.noreply.github.com>
2025-07-15 17:11:24 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,6 @@
 #include "ai_bmt_gui_caller.h"
 #include "ai_bmt_interface.h"
-#include <thread>
-#include <chrono>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <unordered_map>
-#include <cpu_provider_factory.h>
-#include <onnxruntime_cxx_api.h>
-#include <opencv2/opencv.hpp>
 #include <filesystem>
-
-using namespace std;
-using namespace cv;
-using namespace Ort;
 
 //[Model Recommendation]
 // The loaded model should be stored as a member variable to be used in the runInference function.
@@ -22,140 +9,81 @@ using namespace Ort;
 //[DataType Recommendation]
 // It is recommended to return data using managed data types (e.g., vector<...>).
 // If you use unmanaged data types such as dynamic arrays (e.g., int* data = new int[...]), you must ensure that they are properly deleted at the end of runInference() definition.
-using BMTDataType = vector<float>;
+using DataType = int*;
 
 // To view detailed information on what and how to implement for "AI_BMT_Interface," navigate to its definition (e.g., in Visual Studio/VSCode: Press F12).
-class ImageClassification_Interface_Implementation : public AI_BMT_Interface
+class Virtual_Submitter_Implementation : public AI_BMT_Interface
 {
-private:
-    Env env;
-    RunOptions runOptions;
-    shared_ptr<Session> session;
-    array<const char *, 1> inputNames;
-    array<const char *, 1> outputNames;
-    MemoryInfo memory_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
-
 public:
     virtual void Initialize(string modelPath) override
     {
-        // session initializer
-        SessionOptions sessionOptions;
-        sessionOptions.SetExecutionMode(ExecutionMode::ORT_SEQUENTIAL);
-        sessionOptions.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_EXTENDED);
-        session = make_shared<Session>(env, modelPath.c_str(), sessionOptions);
-
-        // Get input and output names
-        AllocatorWithDefaultOptions allocator;
-        AllocatedStringPtr inputName = session->GetInputNameAllocated(0, allocator);
-        AllocatedStringPtr outputName = session->GetOutputNameAllocated(0, allocator);
-        inputNames = {inputName.get()};
-        outputNames = {outputName.get()};
-        inputName.release();
-        outputName.release();
+        //load the model here
     }
 
     virtual Optional_Data getOptionalData() override
     {
         Optional_Data data;
-        data.cpu_type = "Intel(R) Core(TM) i5-14500";     // e.g., Intel i7-9750HF
-        data.accelerator_type = "";                       // e.g., DeepX M1(NPU)
-        data.submitter = "Jonghyun CAPP Lab PC";          // e.g., DeepX
-        data.cpu_core_count = "14";                       // e.g., 16
-        data.cpu_ram_capacity = "";                       // e.g., 32GB
-        data.cooling = "";                                // e.g., Air, Liquid, Passive
-        data.cooling_option = "";                         // e.g., Active, Passive (Active = with fan/pump, Passive = without fan)
+        data.cpu_type = ""; // e.g., Intel i7-9750HF
+        data.accelerator_type = ""; // e.g., DeepX M1(NPU)
+        data.submitter = ""; // e.g., DeepX
+        data.cpu_core_count = ""; // e.g., 16
+        data.cpu_ram_capacity = ""; // e.g., 32GB
+        data.cooling = ""; // e.g., Air, Liquid, Passive
+        data.cooling_option = ""; // e.g., Active, Passive (Active = with fan/pump, Passive = without fan)
         data.cpu_accelerator_interconnect_interface = ""; // e.g., PCIe Gen5 x16
-        data.benchmark_model = "";                        // e.g., ResNet-50
-        data.operating_system = "Windows";                // e.g., Ubuntu 20.04.5 LTS
+        data.benchmark_model = ""; // e.g., ResNet-50
+        data.operating_system = "Ubuntu 24.04.5 LTS"; // e.g., Ubuntu 20.04.5 LTS
         return data;
     }
 
-    virtual VariantType convertToPreprocessedDataForInference(const string &imagePath) override
+    virtual VariantType convertToPreprocessedDataForInference(const string& imagePath) override
     {
-        Mat image = imread(imagePath);
-        if (image.empty())
-        {
-            throw runtime_error("Failed to load image: " + imagePath);
-        }
-
-        // convert BGR to RGB before reshaping
-        cvtColor(image, image, cv::COLOR_BGR2RGB);
-
-        // reshape (3D -> 1D)
-        image = image.reshape(1, 1);
-
-        // uint_8, [0, 255] -> float, [0 and 1] => Normalize number to between 0 and 1, Convert to vector<float> from cv::Mat.
-        vector<float> vec;
-        image.convertTo(vec, CV_32FC1, 1. / 255);
-
-        // Mean and Std deviation values
-        const vector<float> means = {0.485, 0.456, 0.406};
-        const vector<float> stds = {0.229, 0.224, 0.225};
-
-        // Transpose (Height, Width, Channel)(224,224,3) to (Chanel, Height, Width)(3,224,224)
-        BMTDataType output;
-        for (size_t ch = 0; ch < 3; ++ch)
-        {
-            for (size_t i = ch; i < vec.size(); i += 3)
-            {
-                float normalized = (vec[i] - means[ch]) / stds[ch];
-                output.emplace_back(normalized);
-            }
-        }
-        return output;
+        DataType data  = new int[200*200];
+        for(int i = 0;i<200*200;i++)
+             data[i]=i;
+        return data;
     }
 
-    virtual vector<BMTResult> runInference(const vector<VariantType> &data) override
+    virtual vector<BMTResult> runInference(const vector<VariantType>& data) override
     {
+         vector<BMTResult> queryResult;
         const int querySize = data.size();
-        vector<BMTResult> results;
-
-        // onnx option setting
-        const array<int64_t, 4> inputShape = {1, 3, 224, 224};
-        const array<int64_t, 2> outputShape = {1, 1000};
-
-        for (int i = 0; i < querySize; ++i)
+        for(int i =0;i<querySize;i++)
         {
-            // Prepare input/output tensors
-            BMTDataType imageVec;
+            DataType realData;
             try
             {
-                imageVec = get<BMTDataType>(data[i]);
+                realData = get<DataType>(data[i]);//Ok
             }
-            catch (const std::bad_variant_access &e)
+            catch (const std::bad_variant_access& e)
             {
-                cerr << "Error: bad_variant_access at index " << i << ". Reason: " << e.what() << endl;
+                cerr << "Error: bad_variant_access at index " << i << ". "<< "Reason: " << e.what() << endl;
                 continue;
             }
-            vector<float> outputData(1000);
-            auto inputTensor = Ort::Value::CreateTensor<float>(memory_info, imageVec.data(), imageVec.size(), inputShape.data(), inputShape.size());
-            auto outputTensor = Ort::Value::CreateTensor<float>(memory_info, outputData.data(), outputData.size(), outputShape.data(), outputShape.size());
 
-            // Run inference
-            session->Run(runOptions, inputNames.data(), &inputTensor, 1, outputNames.data(), &outputTensor, 1);
-
-            // Update results
             BMTResult result;
+            vector<float> outputData(1000, 0.1);
             result.classProbabilities = outputData;
-            results.push_back(result);
-        }
+            queryResult.push_back(result);
 
-        return results;
+            delete[] realData; //Since realData was created as an unmanaged dynamic array in convertToData(..) in this example, it should be deleted after being used as below.
+        }
+        return queryResult;
     }
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    filesystem::path exePath = filesystem::absolute(argv[0]).parent_path(); // Get the current executable file path
-    filesystem::path model_path = exePath / "Model" / "Classification" / "mobilenet_v2_opset10.onnx";
+    filesystem::path exePath = filesystem::absolute(argv[0]).parent_path();// Get the current executable file path
+    filesystem::path model_path = exePath / "Model" / "Classification" / "resnet50_opset10.onnx";
     string modelPath = model_path.string();
     try
     {
-        shared_ptr<AI_BMT_Interface> interface = make_shared<ImageClassification_Interface_Implementation>();
+        shared_ptr<AI_BMT_Interface> interface = make_shared<Virtual_Submitter_Implementation>();
         AI_BMT_GUI_CALLER caller(interface, modelPath);
         return caller.call_BMT_GUI(argc, argv);
     }
-    catch (const exception &ex)
+    catch (const exception& ex)
     {
         cout << ex.what() << endl;
     }
